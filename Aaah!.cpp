c32b5c179058ee3536e726ce98777d7ac51a5e45
rafiullah007 <136167854+rafiullah007@users.noreply.github.com>
2023-12-25 00:17:45 +0600

    Add files via upload

---
+++ Aaah!.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    string a;
+    string b;
+    cin>>a;
+    cin>>b;
+    if (a.length() < b.length()) cout << "no" << endl;
+    else cout << "go" << endl;
+
+   return 0;
+}
