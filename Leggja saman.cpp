77f4e2a32b789292d60dc268c882e6501b4bbc69
rafiullah007 <136167854+rafiullah007@users.noreply.github.com>
2023-12-25 14:22:41 +0600

    Update Leggja saman.cpp

--- Leggja saman.cpp
+++ Leggja saman.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
 int sum(int a, int b)
 {
     return a+b;
