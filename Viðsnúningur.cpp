7b593e33973df4dfa670735326851a4fc1ae167b
rafiullah007 <136167854+rafiullah007@users.noreply.github.com>
2023-12-25 15:50:42 +0600

    Add files via upload

---
+++ Viðsnúningur.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+int main()
+{
+    string s;
+    cin>>s;
+    reverse(s.begin(), s.end());
+    cout<<s<<endl;
+}
+
+
