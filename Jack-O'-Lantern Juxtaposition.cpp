7f885e243c921cf0729a189f79c972786f120373
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2023-12-27 09:36:29 -0800

    Add files via upload

---
+++ Jack-O'-Lantern Juxtaposition.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a*b*c<<endl;
+
+}
+
