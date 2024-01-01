04ee0b033bc54cb26a86ab8586e41b8aefc2da55
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-02 01:00:57 +0600

    Add files via upload

---
+++ Reduplication.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    string s;
+    int n ;
+    cin>>s;
+    cin>>n;
+    while(n--)
+    {
+        cout<<s;
+    }
+
+    return 0;
+}
+
