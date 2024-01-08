f482a28e95317337a6642220c98a6caa52f91f35
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-08 23:31:49 +0600

    Add files via upload

---
+++ Flatbökuveisla.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a%b<<endl;
+}
