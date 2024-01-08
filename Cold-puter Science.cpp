5f732d1f055eaa85fdb4a043b48fc4e2a864ea26
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-08 23:08:16 +0600

    Add files via upload

---
+++ Cold-puter Science.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    int t,count(0);
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n<0)
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}
