909ec1844888b082f9621daa19a3bda08cca3283
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-09 20:34:51 +0600

    Add files via upload

---
+++ Amerískur vinnustaður.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    double n = 0.09144;
+    double ans = a*n;
+    printf("%lf",ans);
+}
