34f173fcaf305ba080730dc8f001974f9735a133
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-11 17:36:47 +0600

    Update 99 Problems.cpp

--- 99 Problems.cpp
+++ 99 Problems.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
 int main()
 {
     int n,price,a,m,i;
