9defeaecf5c3856ef83b610b7ab057b352448568
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-02-15 19:29:55 +0600

    Add files via upload

---
+++ A Different Problem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+using namespace std;
+
+int main()
+ {
+    long long int num1, num2;
+
+    while (cin >> num1 >> num2) {
+
+        cout << abs(num1 - num2) << endl;
+    }
+
+    return 0;
+}
