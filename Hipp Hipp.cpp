21cb763e8b7ead924d16ae8bae03a508161eebb8
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-08-23 14:02:16 +0600

    Add files via upload

---
+++ Hipp Hipp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main ()
+{
+    for (int i = 0 ; i<20; i++){
+        cout<<"Hipp hipp hurra!"<<endl;
+    }
+}
